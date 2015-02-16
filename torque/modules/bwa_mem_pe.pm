18bd88de4e72d3b5f647aaa72d94247a9303e529
migbro <miguel.a.brown@gmail.com>
2015-02-16 15:19:11 -0600

    Cleanup
    Reworked and reorganized these scripts

--- torque/modules/bwa_mem_pe.pm
+++ torque/modules/bwa_mem_pe.pm
@@ -24,10 +24,10 @@ sub print {
   open SCRIPT, '>', $script or die $!;
 
   print SCRIPT "#!/usr/bin/env bash\n";
-  print SCRIPT "#PBS -N $main::JOB_NAME.$task\n";
-  print SCRIPT "#PBS -l nodes=1:ppn=8\n";
-  print SCRIPT "#PBS -j oe\n";
-  print SCRIPT "#PBS -o $main::LOGS_DIR\n";
+#  print SCRIPT "#PBS -N $main::JOB_NAME.$task\n";
+#  print SCRIPT "#PBS -l nodes=1:ppn=8\n";
+#  print SCRIPT "#PBS -j oe\n";
+#  print SCRIPT "#PBS -o $main::LOGS_DIR\n";
   print SCRIPT "cd \$PBS_O_WORKDIR\n";
   print SCRIPT "($main::BWA mem -t \$PBS_NUM_PPN -R \"$RGRP\" -v 2 $main::BWA_REF_FA $f1 $f2 | $main::SAMTOOLS view -bT $main::SAM_REF_FA - > $main::sample.bam) > $main::LOGS_DIR/$main::sample.bwa.pe.log 2>&1\n";
 
