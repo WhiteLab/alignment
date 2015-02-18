97ed77a99e4d2caf46bbb09d133069f59f1f3ab7
migbro <miguel.a.brown@gmail.com>
2015-02-18 12:04:19 -0600

    Added upload to swift and file cleanup step to pipeline, as well as naming conventions for attached drives.

---
+++ osdc/modules/upload_to_swift.py
@@ -0,0 +1,34 @@
+import sys
+from date_time import date_time
+from subprocess import call
+
+def upload_to_swift(bid,sample):
+    ONE_GB = 1073741824
+    setup_cmd="source /home/ubuntu/.novarc"
+    call(setup_cmd, shell=True)
+    sf_rm="rm " + sample + "_1_sequence.txt.gz " + sample + "_2_sequence.txt.gz"
+#    call(sf_rm, shell=True)
+    swift_cmd="swift upload PANCAN ./ --object-name ALIGN_TEST/" + bid + " -S " + str(ONE_GB)
+    sys.stderr.write(date_time() + swift_cmd + "\n")
+#    call(swift_cmd,shell=True)
+    cleanup_cmd="rm *.qs *bam* *.hist *.metrics log.txt *.bai"
+    sys.stderr.write(date_time() + cleanup_cmd + "\n")
+    call(cleanup_cmd, shell=True)
+    dir_clean_cmd="rm -rf LOGS picard_tmp"
+    sys.stderr.write(date_time() + dir_clean_cmd + "\n")
+    call(dir_clean_cmd,shell=True)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser=argparse.ArgumentParser(description='Last poart of pipeline.  Uploads results to swift and clears current volume')
+    parser.add_argument('-id','--bid',action='store',dest='bid',help='Bionimbus id')
+    parser.add_argument('-sa','--sample',action='store',dest='sample',help='Sample/project name prefix')
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+
+    inputs=parser.parse_args()
+    (bid,sample)=(inputs.bid,inputs.sample)    
+    upload_to_swift(bid,sample)
