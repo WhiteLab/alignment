c8f9ea2370dc75a70e11fd9b5d28d7879ab82335
migbro <miguel.a.brown@gmail.com>
2015-02-20 16:58:53 -0600

    Created control vm script to spawn pipeline vms and attach cinder volumes with references to them

--- osdc/basic_node_setup.py
+++ osdc/basic_node_setup.py
@@ -8,8 +8,10 @@ import re
 import argparse
 from date_time import date_time
 import subprocess
+from setup_vm import setup_vm
+from attach_cinder import attach_cinder
 
-parser=argparse.ArgumentParser(description='VM spawner for pipeline processes')
+parser=argparse.ArgumentParser(description='VM spawner for pipeline processes.  Sets up vm for sample analysis and attaches sotrage references')
 parser.add_argument('-id','--BID',action='store',dest='bid',help='Project Bionimbus ID')
 parser.add_argument('-im','--image',action='store',dest='image',help='Image id to spawn')
 parser.add_argument('-w','--wait',action='store',dest='wait',help='Wait time before giving up on spawning an image.  Reommended value 300 (in seconds)')
@@ -24,46 +26,12 @@ bid=inputs.bid
 image=inputs.image
 wait=inputs.wait
 
-sys.stderr.write(date_time() + "Starting vm qc for sample set " + bid + "\n")
-# need build variables to call nova successfully
-src_cmd='source /home/ubuntu/.novarc'
-subprocess.call(src_cmd,shell=True)
-vm = "vm_pipe_" + bid
-vm_boot="nova boot " + vm + " --image " + image + " --flavor 5 --key_name mb_inst"
-subprocess.call(vm_boot, shell=True)
-
-# check status of vm until finshed spawing every 30s
-i=30
-sleep='sleep ' + str(i) + 's'
-n=i
-subprocess.call(sleep, shell=True)
-flag=0
-test='DNAseq_spawner_dev'
-vip=''
-vid=''
-while flag==0:
-    if n > wait:
-        break
-    else:
-        sys.stderr.write(date_time() + "Checking success of vm boot. " + str(n) + " seconds have passed\n")
-        check='nova list'
-        p=subprocess.check_output(check,shell=True)
-        for line in re.findall('(.*)\n',p):
-            line=line.rstrip('\n')
-            if(re.search(vm,line)):
-                line=re.sub(r"\|",r"",line)
-                info=line.split()
-                vid=info[0]
-                vname=info[1]
-                vstatus=info[2]
-                sys.stderr.write('Status of ' + vname + ' is ' + vstatus + ' with id ' + vid + '\n')
-                if(vstatus=="ACTIVE"):
-                    flag=1
-                    m=re.search("private=(.*)",info[5])
-                    vip=m.group(1)
-                    break
-        n=(n+i)
-if(flag==1):
-    sys.stderr.write("VM setup for " + vm + " with IP address " + vip + " with ID " + vid + " successful\n")
+(flag,vid,vip)=setup_vm(bid,image,wait)
+if flag==1:
+    #default sid  change here for a differenet reference volume
+    sid='18c83d7d-dd26-4fa3-8f35-ff378c580c27'
+    sys.stderr.write("Attaching cinder volume...\n")
+    vol=attach_cinder(sid,vid,bid,wait)
+    sys.stderr.write("Volume attached.  Go play and have fun!\n")
 else:
     sys.stderr.write("VM setup time out for " + vm + "Check connection settings or increase wait time and try again\n")
