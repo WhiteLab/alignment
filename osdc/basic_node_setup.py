2189d092d33f842519850e36a182457d03c6d739
migbro <miguel.a.brown@gmail.com>
2015-02-23 12:21:26 -0600

    Moved vm set up modules to utility.  Added ability to use json config files for vm setup

--- osdc/basic_node_setup.py
+++ osdc/basic_node_setup.py
@@ -2,36 +2,58 @@
 # written by Miguel Brown 2015-Feb-17. Sets up a pipeline vm, creates cinder blocks, and attaches
 
 import sys
+sys.path.append('/home/ubuntu/TOOLS/Scripts/utility')
 sys.path.append('/home/ubuntu/TOOLS/Scripts/modules')
 import os
 import re
+import json
 import argparse
 from date_time import date_time
 import subprocess
 from setup_vm import setup_vm
 from attach_cinder import attach_cinder
 
-parser=argparse.ArgumentParser(description='VM spawner for pipeline processes.  Sets up vm for sample analysis and attaches sotrage references')
-parser.add_argument('-id','--BID',action='store',dest='bid',help='Project Bionimbus ID')
-parser.add_argument('-im','--image',action='store',dest='image',help='Image id to spawn')
-parser.add_argument('-w','--wait',action='store',dest='wait',help='Wait time before giving up on spawning an image.  Reommended value 300 (in seconds)')
+class Setup():
 
-if len(sys.argv)==1:
-    parser.print_help()
-    sys.exit(1)
+    def __init__(self, bid,json_config,wait):
+        self.json_config = json_config
+        self.wait=wait
+        self.bid=bid
+        self.parse_config()
+        
+    def parse_config(self):
+        self.config_data = json.loads(open(self.json_config, 'r').read())
+        (self.vm_id,self.flavor,self.key)=(self.config_data['vm_image']['id'],self.config_data['vm_image']['flavor'],self.config_data['vm_image']['key'])
+        self.create_vm()
 
-inputs=parser.parse_args()
+    def create_vm(self):
+        (flag,vid,vip)=setup_vm(self.bid,self.vm_id,self.flavor,self.key,self.wait)
+        vm = "vm_pipe_" + self.bid
+        if flag==1:
+            sid=self.config_data['cinder_ref']['id']
+            size=self.config_data['cinder_ref']['size']
+            sys.stderr.write("Attaching cinder volume...\n")
+            vol=attach_cinder(sid,vid,self.bid,size,self.wait)
+            sys.stderr.write("Volume attached.  Go play and have fun!\n")
+        else:
+            sys.stderr.write("VM setup time out for " + vm + "Check connection settings or increase wait time and try again\n")
 
-bid=inputs.bid
-image=inputs.image
-wait=inputs.wait
 
-(flag,vid,vip)=setup_vm(bid,image,wait)
-if flag==1:
-    #default sid  change here for a differenet reference volume
-    sid='18c83d7d-dd26-4fa3-8f35-ff378c580c27'
-    sys.stderr.write("Attaching cinder volume...\n")
-    vol=attach_cinder(sid,vid,bid,wait)
-    sys.stderr.write("Volume attached.  Go play and have fun!\n")
-else:
-    sys.stderr.write("VM setup time out for " + vm + "Check connection settings or increase wait time and try again\n")
+def main():
+    parser=argparse.ArgumentParser(description='VM spawner for pipeline processes.  Sets up vm for sample analysis and attaches sotrage references')
+    parser.add_argument('-id','--BID',action='store',dest='bid',help='Project Bionimbus ID')
+    parser.add_argument('-j','--json',action='store',dest='config_file',help='JSON config file with snapshot ids and set up params')
+    parser.add_argument('-w','--wait',action='store',dest='wait',help='Wait time before giving up on spawning an image.  Reommended value 300 (in seconds)')
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+
+    inputs=parser.parse_args()
+
+    bid=inputs.bid
+    config_file=inputs.config_file
+    wait=inputs.wait                    
+    setup=Setup(bid,config_file,wait)
+if __name__ == '__main__':
+  main()
