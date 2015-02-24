61c1ab4fa7ff5e2fd293ab3690f00cefd31b3da9
migbro <miguel.a.brown@gmail.com>
2015-02-24 17:46:37 -0600

    Added config file for pipeline.  Made pipeline a module for a wrapper script to take a list of samples.  Modified pipeline being tested, wrapper script needs error checking.  Directory cleanup in upload_swift disabled for now

--- osdc/modules/upload_to_swift.py
+++ osdc/modules/upload_to_swift.py
@@ -4,18 +4,17 @@ from subprocess import call
 
 def upload_to_swift(bid,sample):
     ONE_GB = 1073741824
-    setup_cmd="source /home/ubuntu/.novarc"
-    call(setup_cmd, shell=True)
+    src_cmd=". /home/ubuntu/.novarc"
     sf_rm="rm " + sample + "_1_sequence.txt.gz " + sample + "_2_sequence.txt.gz"
-    call(sf_rm, shell=True)
+#    call(sf_rm, shell=True)
     p_tmp_rm="rm -rf picard_tmp"
     call(p_tmp_rm,shell=True)
-    swift_cmd="swift upload PANCAN ./ --object-name ALIGN_TEST/" + bid + " -S " + str(ONE_GB)
+    swift_cmd=src_cmd + "swift upload PANCAN ./ --object-name ALIGN_TEST/" + bid + " -S " + str(ONE_GB)
     sys.stderr.write(date_time() + swift_cmd + "\n")
     call(swift_cmd,shell=True)
     cleanup_cmd="rm *.qs *bam* *.hist *.metrics *.txt *.bai"
-    sys.stderr.write(date_time() + cleanup_cmd + "\n")
-    call(cleanup_cmd, shell=True)
+#    sys.stderr.write(date_time() + cleanup_cmd + "\n")
+#    call(cleanup_cmd, shell=True)
 
 if __name__ == "__main__":
     import argparse
