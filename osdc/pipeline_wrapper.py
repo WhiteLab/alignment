d4ca3c9973cc806b2799bb6180deb29ef9a1754f
migbro <miguel.a.brown@gmail.com>
2015-02-26 17:32:20 -0600

    Pipeline wrapper can now use an input file to serially process read groups using pipeline.  TO DO:  Fix logging behavior.

--- osdc/pipeline_wrapper.py
+++ osdc/pipeline_wrapper.py
@@ -8,11 +8,12 @@ import re
 import argparse
 from date_time import date_time
 import subprocess
+from download_from_swift import download_from_swift
+from pipeline import Pipeline
 
-parser=argparse.ArgumentParser(description='Pipeline wrapper script to process multiple paired end set serially')
+parser=argparse.ArgumentParser(description='Pipeline wrapper script to process multiple paired end set serially.')
 parser.add_argument('-f','--file',action='store',dest='fn',help='File with bionimbus ID, seqtype and sample list')
-#parser.add_argument('-f2','--file2',action='store',dest='end2',help='Second fastq file')
-#parser.add_argument('-t','--seqtype',action='store',dest='seqtype',help='Type of sequencing peformed.  Likely choices are genome, exome, and target for capture')
+
 if len(sys.argv)==1:
     parser.print_help()
     sys.exit(1)
@@ -21,39 +22,110 @@ inputs=parser.parse_args()
 fh=open(inputs.fn,'r')
 src_cmd='. ~/.novarc;'
 obj='PANCAN'
-
-pipe_tool='/home/ubuntu/TOOLS/Scripts/modules/pipeline.py'
-
+cont='ALIGN_TEST'
+# paired-end config file to use for pipeline
+pipe_cfg='/home/ubuntu/TOOLS/Scripts/utility/hg19_pe_config.json'
+# dictionary to track status of success of pipelines for each sample and lane to help troubleshoot any failures
+samp_status={}
 for line in fh:
     line=line.rstrip('\n')
     (bid,seqtype,samp_csv)=line.split('\t')
+    cwd='/mnt/cinder/REFS_' + bid + '/SCRATCH'
+    sys.stderr.write(date_time() + 'Initializing scratch directory for ' + bid + '\n')
+    # All files for current bid to be stored in cwd
+    check_dir=os.path.isdir(cwd)
+    if check_dir==False:
+        subprocess.check_output('mkdir ' + cwd,shell=True)
+    try:
+        os.chdir(cwd)
+    except:
+        sys.stderr.write(date_time() + 'Creating directory for ' + bid + ' failed. Ensure correct machine being used for this sample set\n')
+        continue
+    
     contain='RAW/' + bid + '/' + bid + '_'
+    cur_dir=cwd + '/RAW/' + bid
     # get list of paired end sequencing files for each pair
     for samp in samp_csv.split(', '):
+        samp_status[samp]={}
+        samp_status[samp]['s']='Initializing'
+        samp_status[samp]['sf']={}
         swift_cmd=src_cmd + 'swift list ' + obj + ' --prefix ' + contain + samp
         sys.stderr.write(date_time() + 'Getting sequece files for sample ' + samp + '\n' + swift_cmd + '\n')
-        contents=subprocess.check_output(swift_cmd,shell=True)
+        try:
+            contents=subprocess.check_output(swift_cmd,shell=True)
+        except:
+            sys.stderr.write(date_time() + 'Can\'t find sequencing files for ' + samp + ' skipping!\n')
+            continue
         end1=''
         end2=''
+        sf1=''
+        sf2=''
         i=0
+        samp_status[samp]['s']='Running'
+        # sequencing files downloaded in pairs using simple iterator, as swift gives files in alphanumeric order - standard file naming should work with this
         for seqfile in re.findall('(.*)\n',contents):
             seqfile=seqfile.rstrip('\n')
             if i==0:
-                end1=seqfile
+                sf1=seqfile
+                end1=os.path.basename(sf1)
                 i=i+1
             else:
-                end2=seqfile
+                sf2=seqfile
+                end2=os.path.basename(sf2)
+                samp_status[samp]['sf'][seqfile]='Downloading'
                 i=0
-                swift_dl_cmd=src_cmd + 'swift download ' + obj + ' ' + end1
-                sys.stderr.write(date_time() + 'Getting first sequencing file\n' + swift_dl_cmd + '\n')
-                subprocess.call(swift_dl_cmd,shell=True)
-                swift_dl_cmd=src_cmd + 'swift download ' + obj + ' ' + end2
-                sys.stderr.write(date_time() + 'Getting second sequencing file\n' + swift_dl_cmd + '\n')
-                subprocess.call(swift_dl_cmd,shell=True)
-                pipe_cmd=pipe_tool + ' -f1 ' + end1 + ' -f2 ' + end2 + ' -t ' + seqtype + '2>> ' 'LOGS/' + samp + '.log >> ' + samp + '.log'
-                sys.stderr.write(date_time() + 'Running pipeline process ' + pipe_cmd + '\n')
-                status=subprocess.call(pipe_cmd,shell=True)
-                if(status != 1):
-                    sys.stderr.write("Pipeline process for sample " + seqfile + " failed!\n")
+                m=re.match('^(\S+)_2_sequence\.txt\.gz$',sf2)
+                lane=m.group(1)
+                # attempt to download sequencing files
+                sys.stderr.write(date_time() + 'Getting sequencing files\n')
+                try:
+                    download_from_swift(obj,lane)
+                except:
+                    sys.stderr.write(date_time() + 'Getting sequencing files ' + sf1 + ' and ' + sf2 + ' failed.  Moving on\n')
+                    samp_status[samp]['sf'][seqfile]='Download failed'
+                    continue
+                # pipeline needs to be run in same directory as sequencing files
+                try:
+                    os.chdir(cur_dir)
+                    l_dir=cur_dir + '/LOGS'
+                    l_check=os.path.isdir(l_dir)
+                    if l_check==False:
+                        subprocess.call('mkdir ' + l_dir, shell=True)
+                except:
+                    sys.stderr.write(date_time() + 'Could not change to new directory ' + cur_dir + ' Skipping and removing sequencing files\n')
+                    rm_sf='rm ' + cur_dir + '/' + sf1 + ' ' + cur_dir + '/' +  sf2
+                    subprocess.call(rm_sf,shell=True)
+                    os.chdir(cwd)
                     exit(3)
-        
+                # if pipeline fails, abandon process as a larger error might come up
+                sys.stderr.write(date_time() + 'Running pipeline process \n')
+                try:
+                    p=Pipeline(end1,end2,seqtype,pipe_cfg)
+                    p.pipeline()
+                    if p==1:
+                        sys.stderr.write("Pipeline process for sample " + seqfile + " failed!\n")
+                        samp_status[samp]['sf'][seqfile]='Pipeline failed'
+                        samp_status[samp]['s']='Failures detected'
+                        for sf in samp_status[samp]['sf']:
+                            sys.stderr.write(samp + '\t' + sf + '\t' + samp_status[samp]['sf'][sf] + '\n')
+                            exit(3)
+                except:
+                    sys.stderr.write("Pipeline process for sample " + seqfile + " failed!\n")
+                    samp_status[samp]['sf'][seqfile]='Pipeline failed'
+                    samp_status[samp]['s']='Failures detected'
+                    for sf in samp_status[samp]['sf']:
+                        sys.stderr.write(samp + '\t' + sf + '\t' + samp_status[samp]['sf'][sf] + '\n')
+                    exit(1)
+                # change back to parent directory so that new sequencing files can be downloaded in same place
+                os.chdir(cwd)
+                samp_status[samp]['sf'][seqfile]='Success!'
+        samp_status[samp]['s']='Succeeded'
+        for sf in samp_status[samp]['sf']:
+            sys.stderr.write(samp + '\t' + sf + '\t' + samp_status[samp]['sf'][sf] + '\n')
+    os.chdir(cur_dir)
+    mv_gz='mv ../*.gz .'
+    subprocess.call(mv_gz,sgell=True)
+    qc_cmd='/home/ubuntu/Scripts/parse_qc.pl 0'
+    subprocess.call(qc_cmd)
+    upload_to_swift(obj,cont)
+sys.stderr.write(date_time() + "Process complete.  Check logs for any errors\n")
