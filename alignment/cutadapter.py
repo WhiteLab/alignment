3303458a23b4d7ccaeb223c5bd83b869e36d88ac
migbro <miguel.a.brown@gmail.com>
2015-09-30 14:46:11 -0500

    Added cutadapt step to pipeline

---
+++ alignment/cutadapter.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import sys
+
+sys.path.append('/home/ubuntu/TOOLS/Scripts/utility')
+import os
+import json
+from date_time import date_time
+from subprocess import call
+from log import log
+
+
+def parse_config(config_file):
+    config_data = json.loads(open(config_file, 'r').read())
+    (cutadapt_tool, qual, mqual) = (
+        config_data['tools']['cutadapt'],
+        config_data['params']['qual'], config_data['params']['mqual'])
+    return (cutadapt_tool, qual, mqual)
+
+
+def cutadapter(sample, end1, end2, config_file):
+    # casual logging - look for a LOGS directory, otherwise assume current dir
+    log_dir = './'
+    if os.path.isdir('LOGS'):
+        log_dir = 'LOGS/'
+    loc = log_dir + sample + '.cutadapt.log'
+    temp1 = end1 + '.temp'
+    temp2 = end2 + '.temp'
+    (cutadapt_tool, qual, mqual) = parse_config(config_file)
+    cutadapt_cmd = cutadapt_tool + ' -m ' + ' --quality-base=' + qual + ' -q ' + mqual + ' -o ' + temp1 + ' -p ' + \
+                   temp2 + ' ' + end1 + ' ' + end2 + ' >> ' + loc + ' 2>> ' + loc
+    log(loc, date_time() + cutadapt_cmd + "\n")
+    call(cutadapt_cmd, shell=True)
+    log(loc, date_time() + 'Quality score trimming complete.  Replacing fastq on working directory.')
+    rn_fq = 'mv ' + temp1 + ' ' + end1 + ' ' + temp2 + ' ' + end2
+    call(rn_fq, shell=True)
+    return 0
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(
+        description='cutadapt module.  Removes Set minimum base quality')
+    parser.add_argument('-sa', '--sample', action='store', dest='sample', help='Sample/location name prefix')
+    parser.add_argument('-f1', '--file1', action='store', dest='end1', help='First of paired-end fastq file')
+    parser.add_argument('-f2', '--file2', action='store', dest='end2', help='Second of paired-end fastq file')
+    parser.add_argument('-j', '--json', action='store', dest='config_file',
+                        help='JSON config file containing tool and reference locations')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    inputs = parser.parse_args()
+    (sample, end1, end2, config_file) = (inputs.sample, inputs.end1, inputs.end2, inputs.config_file)
+    cutadapter(sample, end1, end2, config_file)
