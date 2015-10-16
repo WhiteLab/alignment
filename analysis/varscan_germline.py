e0f229eee0f1a543c686ddcce66c02b74953a24b
migbro <miguel.a.brown@gmail.com>
2015-10-16 11:19:43 -0500

    Corrected odd int cast

--- analysis/varscan_germline.py
+++ analysis/varscan_germline.py
@@ -9,7 +9,7 @@ from job_manager import job_manager
 
 def parse_config(config_file):
     config_data = json.loads(open(config_file, 'r').read())
-    return config_data['tools']['samtools'], int(config_data['tools']['varscan']), config_data['references']['fai'], \
+    return config_data['tools']['samtools'], config_data['tools']['varscan'], config_data['references']['fai'], \
            config_data['references']['fa_ordered'], config_data['params']['threads']
 
 
