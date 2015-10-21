a7b6c91f8eb923dbfd88736c6b47b289f307af3b
Ubuntu <ubuntu@vm-pipe-2015-682.novalocal>
2015-10-21 21:45:12 +0000

    Added germline varscan script

---
+++ analysis/concat_vcfs2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""
+Usage: ./concat_vcfs2.py <fai> <dir>
+
+Arguments:
+<fai> fasta index
+<dir>  directory vcfs
+
+Options:
+-h
+"""
+from docopt import docopt
+import sys
+args = docopt(__doc__)
+chrs = open(args['<fai>'],'r')
+header_flag = 0
+for entry in chrs:
+    entry = entry.rstrip('\n')
+    chrom = entry.split('\t')
+    vcf = open(args['<dir>'] + '/' + chrom[0] + '.vcf', 'r')
+    for line in vcf:
+        line = line.strip()
+        if (line.startswith('#') and header_flag == 0):
+            print line
+        elif (not line.startswith('#')):
+            print line
+    header_flag = 1
