c901510bb62c124e880900331f82d2fea28a5f59
migbro <miguel.a.brown@gmail.com>
2015-12-21 11:27:11 -0600

    Modified helper script to take a list of vcfs and be more flexible with file input names

--- analysis/concat_vcfs2.py
+++ analysis/concat_vcfs2.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 Usage: ./concat_vcfs2.py <fai> <dir>
 
 Arguments:
 <fai> fasta index
-<dir>  directory vcfs
+<list>  vcfs list
 
 Options:
 -h
@@ -14,14 +14,30 @@ from docopt import docopt
 args = docopt(__doc__)
 chrs = open(args['<fai>'], 'r')
 header_flag = 0
+header = []
+vcf_entries = {}
+order = []
+import re
+
 for entry in chrs:
     entry = entry.rstrip('\n')
     chrom = entry.split('\t')
-    vcf = open(args['<dir>'] + '/' + chrom[0] + '.vcf', 'r')
-    for line in vcf:
-        line = line.strip()
-        if line.startswith('#') and header_flag == 0:
-            print line
-        elif not line.startswith('#'):
-            print line
+    vcf_entries[chrom[0]] = []
+    order.append(chrom[0])
+
+vcf = open(args['<dlist>'], 'r')
+for line in vcf:
+    line = line.strip()
+    m = re.search('(chr\S+)\.', line)
+    cur = m.group(1)
+    for variant in line:
+        if variant.startswith('#') and header_flag == 0:
+            header.append(variant)
+        elif not variant.startswith('#'):
+            vcf_entries[cur].append(variant)
     header_flag = 1
+vcf.close()
+print header
+for chrom in order:
+    print vcf_entries[chrom]
+
