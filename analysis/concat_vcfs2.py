0a04252aed098d25cec5fb249f2f4632b0bf3bed
migbro <miguel.a.brown@gmail.com>
2015-12-21 11:36:04 -0600

    Don't look at me, I'm hideous

--- analysis/concat_vcfs2.py
+++ analysis/concat_vcfs2.py
@@ -25,18 +25,20 @@ for entry in chrs:
     vcf_entries[chrom[0]] = []
     order.append(chrom[0])
 
-vcf = open(args['<list>'], 'r')
-for line in vcf:
+vcf_list = open(args['<list>'], 'r')
+for line in vcf_list:
     line = line.strip()
     m = re.search('(chr[A-Z,a-z,0-9,_]+)', line)
     cur = m.group(1)
-    for variant in line:
+    vcf = open(line, 'r')
+    for variant in vcf:
         if variant.startswith('#') and header_flag == 0:
             header.append(variant)
         elif not variant.startswith('#'):
             vcf_entries[cur].append(variant)
     header_flag = 1
-vcf.close()
+    vcf.close()
+vcf_list.close()
 print header
 for chrom in order:
     print vcf_entries[chrom]
