913791cdbd026fd83d4726093aaf66614277702e
migbro <miguel.a.brown@gmail.com>
2016-01-15 17:50:48 -0600

    shell seems to be running string of commands all at once, trying piecemeal instead

--- utility/qc_various/metalfox_pipe.py
+++ utility/qc_various/metalfox_pipe.py
@@ -28,12 +28,15 @@ def metalfox_pipe(config_file, sample_pairs, ref_mnt):
                        + ' | grep .rmdup.srt.ba* '
         bam =subprocess.check_output(get_bam_name, shell=True).split('\n')
         dl_bam = 'swift download ' + cont + ' ' + bam[0] + ' ' + bam[1] + ';'
+        job_list.append(src_cmd + dl_bam)
         mut_out = 'ANALYSIS/' + info[0] + '/OUTPUT/' + info[0] + '.out.keep'
         dl_out = 'swift download ' + cont + ' ' + mut_out + ';'
+        job_list.append(src_cmd + dl_out)
         run_metal = metalfox_tool + ' -f1 ' + mut_out + ' -f3 ' + bam[0] + ' -m ' + map_ref + ' > ' + info[0] + \
                     '.foxog_scored_added.out;'
+        job_list.append(src_cmd + run_metal)
         cleanup = 'rm ' + ' '.join((bam[0],bam[1],mut_out)) + ';'
-        job_list.append(src_cmd + dl_bam + dl_out + run_metal + cleanup)
+        job_list.append(src_cmd + cleanup)
     pairs.close()
     job_manager(job_list, max_t)
 
