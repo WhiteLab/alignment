da4a66318813cd76ef418eba9641d19524c8e601
Ubuntu <ubuntu@vm-pipe-pancan-cc.novalocal>
2016-01-25 19:43:58 +0000

    Bug fixes

--- utility/garvin2hgac.py
+++ utility/garvin2hgac.py
@@ -16,6 +16,7 @@ Options
 
 '''
 import sys
+import pdb
 
 sys.path.append('/home/ubuntu/TOOLS/Scripts/utility')
 from docopt import docopt
@@ -25,23 +26,26 @@ args = docopt(__doc__)
 lut = {}
 for line in open(args['<LUT>'], 'r'):
     ids = line.rstrip('\n').split('\t')
+
     lut[ids[0]] = ids[1]
 
 job_list = []
 ONE_GB = 1073741824
 src_cmd = '. /home/ubuntu/.novarc;'
-swift_cmd = src_cmd + 'swift upload ' + args['cont'] + ' --skip-identical -S ' + str(ONE_GB) + ' --object-name '
+swift_cmd = src_cmd + 'swift upload ' + args['<cont>'] + ' --skip-identical -S ' + str(ONE_GB) + ' --object-name '
 
 for fq in open(args['<fq_list>'],'r'):
     info = fq.rstrip('\n').split('_')
-    proj_id = fq[2] + '_' + fq[3]
+    proj_id = info[2] + '_' + info[3]
+    pdb.set_trace()
     if proj_id not in lut:
         sys.stderr.write('Could not resolve project id in file name. Skipping ' + fq)
     else:
         bid = lut[proj_id]
-        end = fq[-1][1]
-        new_name = 'RAW/' + bid + '/' + bid + '_' + fq[2] + '_GARVIN_0000_' + fq[0] + '_' + fq[1] + '_' + end\
+        end = info[-1][1]
+        new_name = 'RAW/' + bid + '/' + bid + '_' + info[2] + '_GARVIN_0000_' + info[0] + '_' + info[1] + '_' + end\
                    + '_sequence.txt.gz'
         sys.stderr.write(new_name + '\tnew object name to be uploaded')
         up_cmd = src_cmd + swift_cmd + new_name + ' 2>> up.log >> up.log'
+        sys.stderr.write(up_cmd + '\n')
         job_list.append(up_cmd)
