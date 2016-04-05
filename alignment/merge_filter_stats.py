66365ebe3dd56c59e11891e15b9eb3bdcbfcf724
migbro <miguel.a.brown@gmail.com>
2016-04-05 15:29:25 -0500

    Added filter stats script

--- alignment/merge_filter_stats.py
+++ alignment/merge_filter_stats.py
@@ -21,18 +21,13 @@ def process_line(fh, stop):
         list_list.append(cur)
     return list_list
 
-def rm_parens(line):
-    line = line.replace('(','').replace(')','')
-    return line
-
 def download_from_swift(cont, obj, lane_list):
     src_cmd = ". /home/ubuntu/.novarc;"
     lanes = open(lane_list, 'r')
     head = ''
     data = []
-    print 'BID\tread group\tmin length enforced\tphred score scheme\tmin score enforced\ttotal starting read pairs(rp)' \
-          '\trp too short\t% rp too short\trp passed\t% rp passed\ttotal starting base pairs(bp)\tread1 bp trimmed' \
-          '\tread2 bp trimmed\t% bp trimmed\ttotal bp written\t% bp written'
+    print 'BID\tread group\ttotal alignment pairs(ap)\t% unambiguous ap\t% ambiguous ap\t% total ap filtered' \
+          '\t%total ap kept'
     for line in lanes:
         line = line.rstrip('\n')
         (bid, seqtype, lane_csv) = line.split('\t')
@@ -46,34 +41,15 @@ def download_from_swift(cont, obj, lane_list):
                 sys.stderr.write(date_time() + "Download of " + obj + " from " + cont + " failed\n")
                 exit(1)
             stat = open(cur, 'r')
-            skip_lines(stat, 1)
+            skip_lines(stat, 3)
             temp = []
-            # pdb.set_trace()
-            params = next(stat)
-            m = re.search('-m (\d+) --quality-base=(\d+) -q (\d+)', params)
-            # min len enforced, scoring scheme, min base quality)
-            temp.extend([m.group(1), m.group(2), m.group(3)])
-            skip_lines(stat, 7)
-            # use generic funtion to parse groups on lines, take what's needed, next group
-            # total reads pairs summary section
-            group = process_line(stat, 5)
-            # pdb.set_trace()
-            temp.append(group[0][4])
-            # skip next lines for now, may want in future if adapter trimming performed
-            group[3][6] = rm_parens(group[3][6])
-            temp.extend(group[3][5:7])
-            group[4][5] = rm_parens(group[4][5])
-            temp.extend(group[4][4:6])
-            skip_lines(stat, 1)
-            group = process_line(stat, 1)
-            temp.append(group[0][3])
-            skip_lines(stat, 2)
             group = process_line(stat, 3)
-            group[0][3] = rm_parens(group[0][3])
-            temp.extend((group[1][2], group[2][2], group[0][3]))
-            group = process_line(stat, 1)
-            group[0][5] = rm_parens(group[0][5])
-            temp.extend((group[0][3], group[0][5]))
+            unamb_pairs_pct = group[0][-1][1:]
+            amb_pairs_pct = group[1][-1][1:]
+            filt = str(1-float(unamb_pairs_pct)-float(amb_pairs_pct))
+            kept = str(float(unamb_pairs_pct) + float(amb_pairs_pct))
+            temp.append((group[0][6], unamb_pairs_pct, amb_pairs_pct, filt, kept))
+
             print bid + '\t' + lane + '\t' + '\t'.join(temp)
             stat.close()
 
@@ -88,7 +64,7 @@ def download_from_swift(cont, obj, lane_list):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Uses pipeline lane list to create a summary table of cutadapt stats')
+    parser = argparse.ArgumentParser(description='Uses pipeline lane list to create a summary table of read filter stats')
     parser.add_argument('-c', '--container', action='store', dest='cont', help='Swift container prefix, i.e. PANCAN')
     parser.add_argument('-o', '--object', action='store', dest='obj',
                         help='Swift object name/prefix, i.e. RAW/2015-1234')
