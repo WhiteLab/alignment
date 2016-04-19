a4447b8a65d31e67c6255a229e9ff3ecfba073da
Ubuntu <ubuntu@cc-variant-calling.novalocal>
2016-04-19 18:59:53 +0000

    Debugging platypus wrapper

--- analysis/platypus_germline.py
+++ analysis/platypus_germline.py
@@ -11,26 +11,24 @@ import json
 def parse_config(config_file, cflag):
     config_data = json.loads(open(config_file, 'r').read())
     if cflag == 'y':
-        return config_data['tools']['platypus'], config_data['refs']['genome'], config_data['params']['threads']
+        return config_data['tools']['platypus'], config_data['refs']['fa_ordered'], config_data['params']['threads']
     else:
-        return config_data['tools']['platypus'], config_data['refs']['genome'], config_data['params']['threads'],\
-               config_file['refs']['intervals'], config_file['params']['min_VAF_GL']
+        return config_data['tools']['platypus'], config_data['refs']['fa_ordered'], config_data['params']['threads'],\
+               config_data['refs']['intervals_0base'], config_data['params']['min_VAF_GL']
 
 
-def platypus_germline(config_file, sample, log_dir, cflag):
+def platypus_germline(config_file, sample, log_dir, cflag, ref_mnt):
     if cflag == 'y':
-        (platypus, fasta, threads) = parse_config(config_file)
+        (platypus, fasta, threads) = parse_config(config_file, cflag)
         platypus_cmd = platypus + " --nCPU=" + threads + " --refFile=" + fasta + " --bamFiles=" + sample \
-                       + ".merged.final.bam -o " + sample + ".germline_calls/vcf > " + log_dir + sample \
+                       + ".merged.final.bam -o " + sample + ".germline_calls.vcf > " + log_dir + sample \
                        + ".platypus.log 2>&1"
     else:
-        (platypus, fasta, threads, region_file, minVAF) = parse_config(config_file)
-        region_list = open(region_file, 'r')
-        region_csv = region_list.read()
-        regions = region_csv.replace('\n', ',')
+        (platypus, fasta, threads, region_file, minVAF) = parse_config(config_file, cflag)
+        regions = ref_mnt + '/' + region_file
         platypus_cmd = platypus + " --nCPU=" + threads + " --refFile=" + fasta + " --bamFiles=" + sample \
                        + ".merged.final.bam -o " + sample + ".germline_calls.vcf --minVarFreq=" + minVAF \
-                       + "--regions=" + regions + " > " + log_dir + sample + ".platypus.log 2>&1"
+                       + " --regions=" + regions + " > " + log_dir + sample + ".platypus.log 2>&1"
     log(log_dir + sample + ".platypus.log", date_time() + platypus_cmd + "\n")
     f = 0
     try:
@@ -51,11 +49,13 @@ if __name__ == "__main__":
     parser.add_argument('-l', '--log', action='store', dest='log_dir', help='LOG directory location')
     parser.add_argument('-f', '--flag', action='store', dest='cflag',
                         help='\'y\' if whole genome, \'n\' if custom capture to call only on-target regions')
+    parser.add_argument('-r', '--ref_mnt', action='store', dest='ref_mnt',
+                        help='Reference drive path - i.e. /mnt/cinder/REFS_XXXX')
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
 
     inputs = parser.parse_args()
-    (config_file, sample, log_dir, cflag) = (inputs.config_file, inputs.sample, inputs.log_dir, inputs.cflag)
-    platypus_germline(config_file, sample, log_dir, cflag)
+    (config_file, sample, log_dir, cflag, ref_mnt) = (inputs.config_file, inputs.sample, inputs.log_dir, inputs.cflag, inputs.ref_mnt)
+    platypus_germline(config_file, sample, log_dir, cflag, ref_mnt)
