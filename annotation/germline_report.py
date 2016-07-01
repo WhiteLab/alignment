0093f7ba125c41d6685b5b1874c24ab61859fbad
migbro <miguel.a.brown@gmail.com>
2016-07-01 12:32:53 -0500

    Nearly completed

--- annotation/germline_report.py
+++ annotation/germline_report.py
@@ -3,16 +3,40 @@ import re
 from pysam import VariantFile
 import sys
 import pdb
+import re
 sys.path.append('/home/ubuntu/TOOLS/Scripts/utility')
 from date_time import date_time
 
 
 def gen_report(vcf):
     vcf_in = VariantFile(vcf)
+    desired = {'Consequence': 0, 'IMPACT': 0, 'SYMBOL': 0, 'Amino_acids': 0, 'Codons': 0, 'BIOTYPE': 0, 'SIFT': 0,
+               'Existing_variation': 0, 'VARIANT_CLASS': 0, 'ExAC_MAF': 0, 'CLIN_SIG': 0, 'SOMATIC': 0, 'PHENO': 0,
+               'CADD_PHRED': 0}
+    desc_string = vcf_in.header.info['ANN'].record.keys()
+    desc_string = desc_string.lstrip('"')
+    desc_string = desc_string.rstrip('"')
+    desc_string = desc_string.replace('Consequence annotations from Ensembl VEP. Format: ', '')
+    f_pos_list = []
+    desc_list = desc_string.split('|')
+    ann_size = len(desc_list)
+    for i in xrange(0, ann_size, 1):
+        if desc_list[i] in desired:
+            f_pos_list.append(i)
+            desired[desc_list[i]] = 1
     for record in vcf_in.fetch():
-        sys.stdout.write('CHROM\tPOS\tREF\tSNP\tTR\tTC\tSYMBOL\tCADD\n')
+        sys.stdout.write('CHROM\tPOS\tREF\tAllele\tTotal Allele Count\tTotal Position Coverage\tConsequence\tIMPACT\t'
+                         'SYMBOL\tBIOTYPE\tAmino_acids\tCodons\tExisting_variation\tVARIANT_CLASS\tSIFT\tExAC_MAF\t'
+                         'CLIN_SIG\tSOMATIC\tPHENO\tCADD_PHRED\n')
         pdb.set_trace()
-        horse = 'nay'
+        common = '\t'.join((record.contig, str(record.pos), record.ref, str(record.alts), record.info['TR'],
+                            record.info['TC']))
+        ann_list = [_.split('|') for _ in record.info['ANN'].split(',')]
+        for ann in ann_list:
+            sys.stdout.write(common)
+            for i in f_pos_list:
+                sys.stdout.write('\t' + ann[i])
+            sys.stdout.write('\n')
 
 
 if __name__ == "__main__":
