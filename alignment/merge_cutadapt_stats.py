f1aa4351c42d9cd010168e73dc8e1d46405b7451
migbro <miguel.a.brown@gmail.com>
2016-07-27 15:06:10 -0500

    Fixed header string

--- alignment/merge_cutadapt_stats.py
+++ alignment/merge_cutadapt_stats.py
@@ -75,9 +75,8 @@ def download_from_swift(cont, obj, lane_list):
     src_cmd = ". /home/ubuntu/.novarc;"
     lanes = open(lane_list, 'r')
     head = ''
-    print 'BID\tread group\ttotal starting read pairs(rp)\t% r1 w/ adapter\t% r2 w/ adapter\trp too short\t' \
-          '% rp too short\trp passed\t% rp passed\ttotal starting base pairs(bp)\tread1 bp trimmed\tread2 bp trimmed' \
-          '\t% bp written'
+    print 'BID\tread group\ttotal starting read pairs(rp)\t% r1 w/ adapter\t% r2 w/ adapter\trp too short\t% rp passed' \
+          '\ttotal starting base pairs(bp)\tread1 bp trimmed\tread2 bp trimmed\t% bp written'
     for line in lanes:
         line = line.rstrip('\n')
         (bid, seqtype, lane_csv) = line.split('\t')
