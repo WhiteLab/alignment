95bef0d3b800db8a02526cb7838e95880b313b02
migbro <miguel.a.brown@gmail.com>
2016-08-16 21:38:13 -0500

    troubleshooting new logic

--- alignment/novosort_merge_pe.py
+++ alignment/novosort_merge_pe.py
@@ -143,6 +143,7 @@ def novosort_merge_pe(config_file, sample_list, wait):
                         exit(1)
                     mv_bam = 'mv ' + bam_list[0] + ' ' + sample + '.merged.final.bam; mv ' + bai + ' ' + sample \
                              + '.merged.final.bam.bai'
+                    log(loc, date_time() + mv_bam)
                     subprocess.call(mv_bam, shell=True)
             except:
                 log(loc, 'Rename for single file failed.  Command was ' + mv_bam + '\n')
