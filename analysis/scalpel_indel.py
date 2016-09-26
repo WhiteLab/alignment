06a91c043708fda203b1b6dfb6770e501f34786e
migbro <miguel.a.brown@gmail.com>
2016-09-26 13:33:10 -0500

    Added scalpel indel caller wrapper.  Can be incorporated into variant calling pipeline

---
+++ analysis/scalpel_indel.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import os
+sys.path.append('/home/ubuntu/TOOLS/Scripts/utility')
+from date_time import date_time
+from subprocess import call
+from log import log
+
+
+
+def parse_config(config_file):
+    config_data = json.loads(open(config_file, 'r').read())
+    return config_data['tools']['scalpel'], config_data['refs']['capture'], config_data['refs']['fa_ordered'], \
+           config_data['params']['threads']
+
+
+def create_sample_list(sample_pairs):
+    sample_list = 'sample_list.txt'
+    fh = open(sample_pairs, 'r')
+    sl = open(sample_list, 'w')
+    temp = {}
+    for line in fh:
+        cur = line.rstrip('\n').split('\t')
+        if cur[1] not in temp:
+            sl.write(cur[1] + '\n')
+            temp[cur[1]] = 1
+        if cur[2] not in temp:
+            sl.write(cur[2] + '\n')
+            temp[cur[2]] = 1
+    sl.close()
+    fh .close()
+    del temp
+
+
+def scalpel_indel(pairs, log_dir, config_file):
+    (bed, fasta, cpus, scalpel) = parse_config(config_file)
+    # use get_merged_bams api
+    if not os.path.isfile('sample_list.txt'):
+        create_sample_list(pairs)
+        sys.stderr.write(date_time() + 'Sample pairs list not created - creating one since this is being run likely '
+                                       'outside of pipeline')
+    fh = open(pairs, 'r')
+    for line in fh:
+        cur = line.rstrip('\n').split('\t')
+        loc = log_dir + cur[0] + '.scalpel.log'
+        tumor_bam = cur[1] + '.merged.final.bam'
+        normal_bam = cur[2] + '.merged.final.bam'
+        scalpel_cmd = scalpel + ' --numprocs ' + cpus + ' --tumor ' + tumor_bam + ' --normal ' + normal_bam + ' --bed '\
+                      + bed + ' --ref ' + fasta + ' 2>> ' + loc
+        log(loc, date_time() + 'Starting indel calls for ' + cur[0] + ' with command:\n' + scalpel_cmd + '\n')
+        check = call(scalpel_cmd, shell=True)
+        if check != 0:
+            sys.stderr.write(date_time() + 'Indel calling failed for pair ' + cur[0] + ' with command:\n' +
+                             scalpel_cmd + '\n')
+        log(loc, + date_time() + 'Indel calling complete for pair ' + cur[0])
+    fh.close()
+    sys.stderr.write(date_time() + 'Indel call completed\n')
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Scalpel indel caller wrapper script.  Samples must have been aligned and bams merged '
+                    'ahead of time')
+    parser.add_argument('-sp', '--sample-pairs', action='store', dest='sample_pairs',
+                        help='Tumor/normal sample pair list')
+    parser.add_argument('-j', '--json', action='store', dest='config_file',
+                        help='JSON config file with tool and ref locations')
+    parser.add_argument('-l', '--log', action='store', dest='log_dir', help='LOG directory location')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    inputs = parser.parse_args()
+    (pairs, log_dir, config_file) = (inputs.pairs, inputs.log_dir, inputs.config_file)
+    scalpel_indel(pairs, log_dir, config_file)
