4d341cc31d4b7afed73ed801ec699018dda21f94
migbro <miguel.a.brown@gmail.com>
2016-10-27 16:01:09 -0500

    Testing new mutect vep module

--- annotation/annot_mutect_vep.py
+++ annotation/annot_mutect_vep.py
@@ -34,14 +34,19 @@ def mutect_annot_vep_pipe(config_file, sample_pairs, ref_mnt):
         mk_log_dir = 'mkdir LOGS'
         subprocess.call(mk_log_dir, shell=True)
         loc = 'LOGS/' + sample + '.vep_anno.log'
-        in_vcf = sample + '.germline_pass.vcf'
-        out_vcf = sample + '.germline_pass.vep.vcf'
+        in_vcf = sample + '.out.keep.vcf'
+        out_vcf = sample + '.out.keep.vep.vcf'
         run_vep = 'perl ' + vep_tool + ' --cache -i ' + in_vcf + ' --vcf -o ' + out_vcf + ' --symbol --vcf_info_field' \
                 ' ANN --canonical --html --variant_class --no_whole_genome --offline --maf_exac --no_whole_genome ' \
                 '--fork ' + threads + ' --fasta ' + fasta + ' --dir_cache ' + vep_cache + '--cache_version ' + vcache \
                 + ' --plugin dbNSFP,' + dbsnp + ' 2>> LOGS/' + sample + '.vep.log >> LOGS/' + sample + '.vep.log;'
         log(loc, date_time() + 'Annotating sample ' + sample + '\n')
         check = subprocess.call(run_vep, shell=True)
+        if check != 0:
+            log(loc, date_time() + 'VEP annotation for ' + sample + ' failed\n')
+            exit(1)
+        else:
+            log(loc, date_time() + 'VEP annotation ' + sample + ' successful!\n')
 
 if __name__ == "__main__":
     import argparse
