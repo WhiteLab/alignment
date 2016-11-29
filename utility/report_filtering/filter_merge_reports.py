0639ab4097b5d90768c14cc72d55ceb883c7b556
migbro <miguel.a.brown@gmail.com>
2016-11-29 17:17:08 -0600

    Added some more filter options

--- utility/report_filtering/filter_merge_reports.py
+++ utility/report_filtering/filter_merge_reports.py
@@ -16,7 +16,7 @@ def update_summary(summary, pair, reason):
     return summary
 
 
-def process_indel_report(pair, report, merged_tbl, banned_tup, summary):
+def process_indel_report(pair, report, merged_tbl, banned_tup, summary, length, pos_gene, min_vaf):
     cur = open(report)
     next(cur)
     maf = 0.01
@@ -28,6 +28,11 @@ def process_indel_report(pair, report, merged_tbl, banned_tup, summary):
             continue
         info = line.rstrip('\n').split('\t')
         vtup = '\t'.join(info[0:4])
+        if float(info[-1]) < min_vaf:
+            summary = update_summary(summary, pair, 'low vaf')
+        if len(info[2]) > length or len(info[3]) > length:
+            summary = update_summary(summary, pair, 'indel len')
+            continue
         if vtup in banned_tup:
             summary = update_summary(summary, pair, 'panel')
             continue
@@ -43,14 +48,17 @@ def process_indel_report(pair, report, merged_tbl, banned_tup, summary):
         if int(info[-3]) + int(info[-2]) < cov:
             summary = update_summary(summary, pair, 'low coverage')
             continue
-        valid = info[6] + '-' + info[0] + '_' + info[1] + '_' + info[2] + '->' + info[3]
+        chr_pos = info[0] + '_' + info[1]
+        if chr_pos not in pos_gene:
+            pos_gene[chr_pos] = info[6]
+        valid = pos_gene[chr_pos] + '-' + info[0] + '_' + info[1] + '_' + info[2] + '->' + info[3]
         if valid not in merged_tbl:
             merged_tbl[valid] = {}
         merged_tbl[valid][pair] = str(float(info[-1]) * 100)
-    return merged_tbl, summary
+    return merged_tbl, summary, pos_gene
 
 
-def process_snv_report(pair, report, merged_tbl, summary):
+def process_snv_report(pair, report, merged_tbl, summary, pos_gene, min_vaf):
     merged_tbl[pair] = {}
     cur = open(report)
     next(cur)
@@ -63,9 +71,11 @@ def process_snv_report(pair, report, merged_tbl, summary):
         if line == '\n':
             continue
         info = line.rstrip('\n').split('\t')
-        valid = info[14] + '-' + info[0] + '_' + info[1] + '_' + info[3] + '->' + info[4]
+        cur_vaf = info[10].rstrip('%')
         if info[-1] == 'OFF':
             summary = update_summary(summary, pair, 'off target')
+        if float(cur_vaf) < min_vaf:
+            summary = update_summary(summary, pair, 'low vaf')
             continue
         if float(info[11]) <= tn:
             summary = update_summary(summary, pair, 'low tn ratio')
@@ -82,20 +92,27 @@ def process_snv_report(pair, report, merged_tbl, summary):
         if int(info[5]) + int(info[6]) < cov or int(info[8]) + int(info[9]) < cov:
             summary = update_summary(summary, pair, 'low coverage')
             continue
+        chr_pos = info[0] + '_' + info[1]
+        if chr_pos not in pos_gene:
+            pos_gene[chr_pos] = info[14]
+        valid = pos_gene[chr_pos] + '-' + info[0] + '_' + info[1] + '_' + info[3] + '->' + info[4]
 
-        vaf = info[10].rstrip('%')
         if valid not in merged_tbl:
             merged_tbl[valid] = {}
-        merged_tbl[valid][pair] = vaf
-    return merged_tbl, summary
+        merged_tbl[valid][pair] = cur_vaf
+    return merged_tbl, summary, pos_gene
 
 
-def filter_merge_reports(reports, panel, num_samp):
+def filter_merge_reports(reports, panel, num_samp, length, vaf):
     banned_tup = {}
     summary = {}
     pairs = []
     pair_dict = {}
-    reasons = ('off target', 'low tn ratio', 'low impact', 'high maf', 'low coverage', 'panel')
+    # dict to arbitrarily choose the first gene name it sees at a position to avoid duplicates as position
+    pos_gene = {}
+    indel_max = int(length)
+    min_vaf = float(vaf)
+    reasons = ('off target', 'low vaf', 'low tn ratio', 'indel len', 'low impact', 'high maf', 'low coverage', 'panel')
     for line in open(panel):
         info = line.rstrip('\n').split('\t')
         banned_tup['\t'.join(info[0:4])] = 0
@@ -110,9 +127,10 @@ def filter_merge_reports(reports, panel, num_samp):
             pairs.append(pair)
             pair_dict[pair] = 1
         if vtype == 'indels':
-            (merged_tbl, summary) = process_indel_report(pair, report, merged_tbl, banned_tup, summary)
+            (merged_tbl, summary, pos_gene) = process_indel_report(pair, report, merged_tbl, banned_tup, summary,
+                                                                   indel_max, pos_gene, min_vaf)
         else:
-            (merged_tbl, summary) = process_snv_report(pair, report, merged_tbl, summary)
+            (merged_tbl, summary, pos_gene) = process_snv_report(pair, report, merged_tbl, summary, pos_gene, min_vaf)
     sum_tbl = open('reject_summary.txt', 'w')
     sum_tbl.write('Pair/reason\t' + '\t'.join(reasons) + '\n')
     for pair in pairs:
@@ -150,11 +168,15 @@ if __name__ == "__main__":
                         help='Panel of normals')
     parser.add_argument('-n', '--number_samples', action='store', dest='num_samp',
                         help='Min number of samples to see a variant to report it')
+    parser.add_argument('-l', '--length', action='store', dest='length',
+                        help='Max indel length')
+    parser.add_argument('-v', '--vaf', action='store', dest='vaf',
+                        help='Min variant allele frequency')
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
 
     inputs = parser.parse_args()
-    (reports, panel, num_samp) = (inputs.reports, inputs.panel, inputs.num_samp)
-    filter_merge_reports(reports, panel, num_samp)
+    (reports, panel, num_samp, length, vaf) = (inputs.reports, inputs.panel, inputs.num_samp, inputs.length, inputs.vaf)
+    filter_merge_reports(reports, panel, num_samp, length, vaf)
