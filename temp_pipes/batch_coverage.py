2bf42353e3019035420e814bacad6b7a917492d4
migbro <miguel.a.brown@gmail.com>
2016-12-22 16:57:14 -0600

    Finished writing coverage

--- temp_pipes/batch_coverage.py
+++ temp_pipes/batch_coverage.py
@@ -3,13 +3,43 @@
 import sys
 sys.path.append('/home/ubuntu/TOOLS/Scripts/')
 from utility.date_time import date_time
-from subprocess import Popen
-from subprocess import call
+import re
+import subprocess
 from utility.job_manager import job_manager
 
 
-def calc_coverage(bedtools2_tool, sample, bedfile):
+def get_bam_name(bnid, src_cmd, cont):
+    list_cmd = src_cmd + 'swift list ' + cont + ' --prefix ALIGN/' + bnid + '/BAM/'
+    sys.stderr.write(date_time() + list_cmd + '\nGetting BAM list\n')
+    flist = subprocess.check_output(list_cmd, shell=True)
+    # Use to check on download status
+    bam = ''
+    bai = ''
+    dl_cmd = ''
+    for fn in re.findall('(.*)\n', flist):
+        # depending on software used to index, .bai extension may follow bam
+        test = re.match('^\S+_\w*\d+\.rmdup.srt.ba[m|i]$', fn) or re.match('^\S+_\w*\d+\.rmdup.srt.bam.bai$', fn)
 
+        if test:
+            dl_cmd += src_cmd + 'swift download ' + cont + ' ' + fn + ';'
+            if fn[-3:] == 'bam':
+                bam = fn
+            else:
+                bai = fn
+    return (dl_cmd, bam, bai)
+
+def calc_coverage(bedtools2_tool, sample, bedfile):
+    src_cmd = '. /home/ubuntu/.novarc;'
+    cont = 'Colon_Cancer_RW_SP'
+    job_list = []
+    for bnid in open(sample):
+        bnid = bnid.rstrip('\n')
+        (dl_cmd, bam, bai) = get_bam_name(bnid, src_cmd, cont)
+        bed_cmd = bedtools2_tool + ' coverage -hist -abam ' + bam + ' -b ' + bedfile + ' > ' + bnid + '.hist;'
+        cleanup = 'rm ' + bam + ' ' + bai + ';'
+        final = dl_cmd + bed_cmd + cleanup
+        job_list.append(final)
+    job_manager(job_list, '8')
 
 if __name__ == "__main__":
     import argparse
