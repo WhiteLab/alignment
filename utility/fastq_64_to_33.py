1cec270a4bfddcc601f05389e913e88998f42b89
migbro <miguel.a.brown@gmail.com>
2018-05-08 11:49:38 -0500

    Updated fastq 64 to 33 converter

--- utility/fastq_64_to_33.py
+++ utility/fastq_64_to_33.py
@@ -15,23 +15,23 @@ import sys
 
 from Bio import SeqIO
 
-sys.path.append('/lustre/beagle2/mbrown/PDX_TEST/alignment')
-sys.path.append('/lustre/beagle2/mbrown/PDX_TEST/utility')
-
 from docopt import docopt
 
 args = docopt(__doc__)
 fn = args['<fastq>']
 fastq = gzip.open(fn, 'rb')
+sys.stderr.write('Processing file ' + fn + '\n')
 indir = os.path.dirname(fn)
 bn = os.path.basename(fn)
 if not os.path.isdir(indir + '/converted'):
     os.mkdir(indir + '/converted')
 out = gzip.open(indir + '/converted/' + bn, 'wb')
-
+sys.stderr.write('Opened output file ' + indir + '/converted/' + bn + '\n')
 # code snippets obtained from https://github.com/vpiro/readtools/blob/master/PHRED_converter.py
 quali = 'fastq-illumina'
 qualo = 'fastq-sanger'
+sys.stderr.write('Converting file\n')
 SeqIO.convert(fastq, quali, out, qualo)
 fastq.close()
 out.close()
+sys.stderr.write('Finished!\n')
